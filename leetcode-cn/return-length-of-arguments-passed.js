3d31e5cb0c9eed87353fddbfff7b741559a094c4
Dylan Jones <me@dylanjones.co>
2026-08-17 21:24:41 -0700

    challenge: leetcode-cn 2703

---
+++ leetcode-cn/return-length-of-arguments-passed.js
@@ -0,0 +1,19 @@
+// Leetcode CN easy - 返回传递的参数的长度
+
+/*
+*/
+
+
+//IMPORTANT!! Submit Code Region Begin(Do not remove this line)
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+//IMPORTANT!! Submit Code Region End(Do not remove this line)
